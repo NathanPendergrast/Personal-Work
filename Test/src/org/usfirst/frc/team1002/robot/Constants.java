365910c56742c96b9a3f95949b91e15a4c624b75
NathanPendergrast <nathanpendergrast@yahoo.com>
2018-01-19 11:02:44 -0500

    Add files via upload

---
+++ Test/src/org/usfirst/frc/team1002/robot/Constants.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team1002.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class Constants {
+	// We will have 4 CIMs running 4 sets of 2 mecanum wheels, each driven by a
+	// VictorSP
+	////
+
+}
